4f2ca56297b7a4e63a45a08527ae1c67523144e1
evgenimus <dvh2010#yandex.ru>
2021-09-28 13:16:44 +0300

    lesson14 part one

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+ alert('1');
+
+function DomElement(selector, height, width, bg, fontSize){
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSizer = fontSize;
+  this.addNewElem = function(text){
+    if (this.selector[0] == '.'){
+    let newDiv = document.createElement('div');
+    newDiv.className = this.selector.slice(1);
+    newDiv.innerHTML = text;
+    newDiv.style.height = this.height;
+    newDiv.style.width = this.width;
+     console.log(newDiv.style);
+    newDiv.style.backgroundColor = bg;
+    newDiv.style.backgroundColor = fontSize;
+    document.body.append(newDiv);
+  }
+
+    if (this.selector[0] == '#'){
+    let newP = document.createElement('p');
+    newP.id = this.selector.slice(1);
+    newP.innerHTML = text;
+    document.body.append(newP);
+  }
+ };
+};
+
+ let odjectEx = new DomElement('.div', '100px', '200px', 'blue', '36px');
+ odjectEx.addNewElem('Любой текст'); 
+
+
+
